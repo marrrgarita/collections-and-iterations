47b6bb6472e91fd96d5ec8f435a5a28a17ee29af
Margarita Bartholomew <margarita.veracruz@gmail.com>
2017-03-29 15:23:50 -0400

    exercise 0: create arrays and hashes

---
+++ exercise.rb
@@ -0,0 +1,20 @@
+#exercise 0
+
+fav_colours = ["blue", "green", "purple", "grey", "red"]
+
+ages = [26, 20, 20, 23, 26]
+
+coin_flip_headts = [true, false, true, true, true]
+
+fav_music = ["Jason Mraz", "Ed Sheeran", "Feist"]
+
+fav_colours_symbols = [:blue, :green, :purple, :grey, :red]
+
+
+definitions = {"cup" ==> "drinking vessel", "spork" ==> "combo of spoon and form", "penguin" ==> "best animal ever"}
+
+fav_movies = {"Rent" ==> 2005, "Moana" ==> 2016, "Big Hero 6" ==> 2014}
+
+cities = {"Ajax" ==> 109600, "Tokyo" ==> 13620000, "Manila" ==> 1652000}
+
+sibling_ages = {"Eric" ==> 26, "Belsie" ==> 20, "Brittney" ==> 23, "Adam" ==> 20, "Margarita" ==> 26}
